b60db337aae3ac13211dda3d4aa8a252dc1bc55a
tetomina <tetomina8911@gmail.com>
2022-07-08 10:10:47 +0900

    (eun) App.js _220708

---
+++ begin-react/src/App.js
@@ -0,0 +1,82 @@
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function App () {
+  const [inputs, setInputs] = useState ({
+    username: '',
+    email: ''
+  });
+  
+  const {username, email} = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [ users, setUsers ] = useState ([
+      {
+          id: 1,
+          username: 'velopert',
+          email: 'public.velopert@gmail.com',
+          active: true
+      },
+      {
+          id: 2,
+          username: 'tester',
+          email: 'tester@naver.com',
+          active: false
+      },
+      { 
+          id: 3,
+          username: 'Eun-young',
+          email: 'tetomina8911@gmail.com',
+          active: false
+      }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    //나중에 구현 할 배열에 항목 추가하는 로직
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  }
+
+  const onRemove = id => {
+    //user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    //=user.id가 id인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        user.id == id ? { ...user, active : !user.active} : user
+        )
+    );
+  }
+
+  return (
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
+  );
+
+}
+
+export default App;
---
+++ begin-react/src/UserList.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+
+function User({user, onRemove, onToggle}) {
+    useEffect( () => {
+        console.log('user 값이 설정됨');
+        console.log(user);
+        return () => {
+            console.log('user 가 바뀌기 전..');
+            console.log(user);
+        };
+    }, []);
+
+    return (
+        <div>
+            <b style={{
+                cursor: 'pointer',
+                color: user.active ? 'green' : 'black'
+            }}
+            onClick = { () => onToggle(user.id) }
+            >
+                {user.username}
+            </b>
+            &nbsp;
+            <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
+        </div>
+    );
+}
+
+function UserList({ users, onRemove, onToggle }) {
+    return (
+        <div>
+            {users.map( user => (
+                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
+            ))}
+        </div>
+    );
+}
+
+export default UserList;
